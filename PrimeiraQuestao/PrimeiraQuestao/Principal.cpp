ee7b9719a7f2662a0948f403570e4d0522607cd9
kleber099 <kleber099@gmail.com>
2017-05-21 20:06:13 -0300

    Add files via upload

---
+++ PrimeiraQuestao/PrimeiraQuestao/Principal.cpp
@@ -0,0 +1,32 @@
+#include "priorityUtils.h"
+
+int main() {
+
+	vector<int> pArray;
+	pArray.push_back(3);
+	pArray.push_back(1);
+	pArray.push_back(2);
+	
+	vector<double> vArray;
+	vArray.push_back(8000.0);
+	vArray.push_back(1000.0);
+	vArray.push_back(2500.0);
+	
+	double minimumVolume = 500.0;
+	double maximumVolume = 8000.0;
+	double currentVolume = 2650.0;
+
+	PriorityUtils pu;
+	vector<Tupla>tuplas = pu.zonesAvailableSupply(pArray, vArray, minimumVolume, maximumVolume, currentVolume);
+
+	for (size_t i = 0; i < tuplas.size(); i++) {
+		int prioridade;
+		double volume;
+		tie(prioridade, volume) = tuplas[i];
+		printf("{ %d : %lf }", prioridade, volume);
+	}
+
+	getchar();
+    return 0;
+}
+
---
+++ PrimeiraQuestao/PrimeiraQuestao/priorityZone.cpp
@@ -0,0 +1,40 @@
+#include "priorityZone.h"
+
+/**
+* Construtor de uma Zona de prioridade
+*
+* @param availableVolume Volume da Zona de Prioridade
+* @param priority Prioridade da Zona de Prioridade
+*/
+PriorityZone::PriorityZone(double availableVolume,
+	int priority) : m_availableVolume(availableVolume),
+	m_priority(priority)
+{
+}
+
+/**
+* Retorna o volume da Zona de prioridade
+* @return Volume da Zona de Prioridade
+*/
+double PriorityZone::getAvailableVolume()
+{
+	return m_availableVolume;
+}
+
+/**
+* Retorna a prioridade da Zona de prioridade
+* @return Prioridade da Zona de Prioridade
+*/
+int PriorityZone::getPriority()
+{
+	return m_priority;
+}
+
+/**
+* Sobreescrevendo o operador <, é utilizadod na função sort, para ordenar por m_priority
+* @param availableVolume Volume da Zona de Prioridade
+*/
+bool PriorityZone::operator < (const PriorityZone& priorityZone) 
+{
+	return (m_priority < priorityZone.m_priority);
+}
